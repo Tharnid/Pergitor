cc67eabb69af81772f81906a00fac05284601878
Tharnid <jeffmsmithdev@gmail.com>
2014-11-02 15:37:32 -0500

    done with webforms

---
+++ aenv/3a/hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from flask.ext.bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    if request.method == 'POST' and 'name' in request.form:
+        name = request.form['name']
+    return render_template('index.html', name=name)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
