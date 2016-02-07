e65f9b23fb05fb7a1e5c3e0ac567ef92391f024a
Tharnid <jeffmsmithdev@gmail.com>
2016-02-07 14:12:41 -0500

    on to model methods

--- tharnit/books/admin.py
+++ tharnit/books/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 # import book object
-from .models import Book
+from .models import Author, Book
 
 
 # Register your models here.
 admin.site.register(Book)
+admin.site.register(Author)
