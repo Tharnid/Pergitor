ffd7b56b8f76e12f03456e2607617615dda85118
Tharnid <jeffmsmithdev@gmail.com>
2016-02-10 22:54:24 -0500

    added blather django project

---
+++ LP/slicing.py
@@ -0,0 +1,15 @@
+s = "The trouble is you think you have time"
+
+print(s[0]) # first position
+
+print(s[5]) # 5th position
+
+print(s[:4]) # slicing stop position
+
+print(s[:4]) # slicing start position
+
+print(s[2:14]) # start and stop position
+
+print(s[2:14:3]) # slicing, start, stop, and step (every three chars)
+
+print(s[:]) # quickest way to make a copy
