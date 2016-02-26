46b2c8ec6a0676b77f4bdb1afd5f7a5c848922d5
Tharnid <jeffmsmithdev@gmail.com>
2016-02-26 15:24:11 -0500

    moving on to views

--- grr/gr/admin.py
+++ grr/gr/admin.py
@@ -4,6 +4,9 @@ from gr.models import Gr
 # Register your models here.
 
 class GrAdmin(admin.ModelAdmin):
-    pass
+    # admin screen
+    list_display = ('text', 'created_on', 'total_likes')
+    list_filter = ['created_on']
+    search_fields = ['text']
 
 admin.site.register(Gr, GrAdmin)
--- grr/gr/models.py
+++ grr/gr/models.py
@@ -10,5 +10,8 @@ class Gr(models.Model):
     # Model Method
     def total_likes(self):
         return self.like_set.count()
+
+    def __unicode__(self):
+        return self.text[:50]    # trims to 50 characters via slice mwhahaha
 class Like(models.Model):
     gr = models.ForeignKey(Gr)
